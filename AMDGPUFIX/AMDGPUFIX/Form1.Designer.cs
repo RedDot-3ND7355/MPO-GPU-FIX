85d402e7c56e4b2a1013843632ece92e85b1c911
xEndless <edmayc7@hotmail.fr>
2022-12-15 16:21:21 -0500

    Initial Release

---
+++ AMDGPUFIX/AMDGPUFIX/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace AMDGPUFIX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialCard1 = new MaterialSkin.Controls.MaterialCard();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialSwitch1 = new MaterialSkin.Controls.MaterialSwitch();
+            this.materialFloatingActionButton2 = new MaterialSkin.Controls.MaterialFloatingActionButton();
+            this.materialFloatingActionButton1 = new MaterialSkin.Controls.MaterialFloatingActionButton();
+            this.materialCard1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialCard1
+            // 
+            this.materialCard1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialCard1.Controls.Add(this.materialLabel2);
+            this.materialCard1.Controls.Add(this.materialLabel1);
+            this.materialCard1.Depth = 0;
+            this.materialCard1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialCard1.Location = new System.Drawing.Point(17, 78);
+            this.materialCard1.Margin = new System.Windows.Forms.Padding(14);
+            this.materialCard1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialCard1.Name = "materialCard1";
+            this.materialCard1.Padding = new System.Windows.Forms.Padding(14);
+            this.materialCard1.Size = new System.Drawing.Size(307, 100);
+            this.materialCard1.TabIndex = 0;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel2.Location = new System.Drawing.Point(17, 57);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(85, 19);
+            this.materialLabel2.TabIndex = 4;
+            this.materialLabel2.Text = "GPU Driver: ";
+            this.materialLabel2.UseAccent = true;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(17, 20);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(40, 19);
+            this.materialLabel1.TabIndex = 3;
+            this.materialLabel1.Text = "GPU: ";
+            this.materialLabel1.UseAccent = true;
+            // 
+            // materialSwitch1
+            // 
+            this.materialSwitch1.AutoSize = true;
+            this.materialSwitch1.Depth = 0;
+            this.materialSwitch1.Location = new System.Drawing.Point(56, 192);
+            this.materialSwitch1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch1.Name = "materialSwitch1";
+            this.materialSwitch1.Ripple = true;
+            this.materialSwitch1.Size = new System.Drawing.Size(224, 37);
+            this.materialSwitch1.TabIndex = 2;
+            this.materialSwitch1.Text = "MultiPlane-Overlays Fix";
+            this.materialSwitch1.UseVisualStyleBackColor = true;
+            this.materialSwitch1.CheckedChanged += new System.EventHandler(this.materialSwitch1_CheckedChanged);
+            // 
+            // materialFloatingActionButton2
+            // 
+            this.materialFloatingActionButton2.AutoSize = true;
+            this.materialFloatingActionButton2.Depth = 0;
+            this.materialFloatingActionButton2.DrawShadows = false;
+            this.materialFloatingActionButton2.Icon = global::AMDGPUFIX.Properties.Resources.outline_restart_alt_black_48dp;
+            this.materialFloatingActionButton2.Location = new System.Drawing.Point(295, 181);
+            this.materialFloatingActionButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFloatingActionButton2.Name = "materialFloatingActionButton2";
+            this.materialFloatingActionButton2.Size = new System.Drawing.Size(56, 60);
+            this.materialFloatingActionButton2.TabIndex = 3;
+            this.materialFloatingActionButton2.UseVisualStyleBackColor = true;
+            this.materialFloatingActionButton2.Visible = false;
+            this.materialFloatingActionButton2.Click += new System.EventHandler(this.materialFloatingActionButton2_Click);
+            // 
+            // materialFloatingActionButton1
+            // 
+            this.materialFloatingActionButton1.Depth = 0;
+            this.materialFloatingActionButton1.Enabled = false;
+            this.materialFloatingActionButton1.Icon = global::AMDGPUFIX.Properties.Resources.outline_file_download_black_48dp;
+            this.materialFloatingActionButton1.Location = new System.Drawing.Point(295, 98);
+            this.materialFloatingActionButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFloatingActionButton1.Name = "materialFloatingActionButton1";
+            this.materialFloatingActionButton1.Size = new System.Drawing.Size(56, 63);
+            this.materialFloatingActionButton1.TabIndex = 1;
+            this.materialFloatingActionButton1.UseVisualStyleBackColor = true;
+            this.materialFloatingActionButton1.Click += new System.EventHandler(this.materialFloatingActionButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(366, 240);
+            this.Controls.Add(this.materialFloatingActionButton2);
+            this.Controls.Add(this.materialSwitch1);
+            this.Controls.Add(this.materialFloatingActionButton1);
+            this.Controls.Add(this.materialCard1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Sizable = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AMD RX 6000 Series GPU Fix";
+            this.materialCard1.ResumeLayout(false);
+            this.materialCard1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialCard materialCard1;
+        private MaterialSkin.Controls.MaterialFloatingActionButton materialFloatingActionButton1;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialFloatingActionButton materialFloatingActionButton2;
+    }
+}
+
---
+++ AMDGPUFIX/AMDGPUFIX/Form1.cs
@@ -0,0 +1,120 @@
+﻿using MaterialSkin.Controls;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Management;
+
+namespace AMDGPUFIX
+{
+    public partial class Form1 : MaterialForm
+    {
+        // Globals
+        string GPUName = "";
+        string GPUVersion = "";
+        static RegistryKey defaultKey = null;
+        bool Ready = false;
+        // End
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadGPUDriverVer();
+            DriverCompare();
+            DetectMPO();
+            Ready= true;
+        }
+
+        //
+        // Detect MultiPlane-Overlay
+        //
+        private void DetectMPO()
+        {
+            RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            defaultKey = localMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\DWM\\", writable: true);
+            if (defaultKey.GetValue("OverlayTestMode") != null)
+            {
+                string val = defaultKey.GetValue("OverlayTestMode").ToString();
+                if (int.TryParse(val, out int result))
+                    if (result == 5)
+                        materialSwitch1.Checked = true;
+            }
+        }
+        //
+        // End
+        //
+
+        //
+        // Driver Compare
+        //
+        private void DriverCompare()
+        {
+            string current = GPUVersion.Replace(".", "");
+            string expected = "3101202910015";
+            materialLabel2.HighEmphasis = (current != expected);
+            materialFloatingActionButton1.Enabled = (current != expected);
+        }
+        //
+        // End
+        //
+
+        //
+        // Load GPU Driver Version
+        //
+        ManagementObjectSearcher gpusearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DisplayConfiguration");
+        ManagementObjectSearcher drvsearcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+        private void LoadGPUDriverVer()
+        {
+            // GPU Name
+            foreach (ManagementObject mo in gpusearcher.Get())
+                foreach (PropertyData property in mo.Properties)
+                    if (property.Name == "DeviceName")
+                        GPUName = property.Value.ToString();
+            // End
+            // GPU Driver
+            foreach (ManagementObject mo in drvsearcher.Get())
+                foreach (PropertyData property in mo.Properties)
+                    if (property.Name == "DriverVersion")
+                        GPUVersion = property.Value.ToString();
+            // End
+            materialLabel1.Text += GPUName;
+            materialLabel2.Text += GPUVersion;
+        }
+        //
+        // End
+        //
+
+        //
+        // Download Latest Driver with Black screen fix
+        //
+        private void materialFloatingActionButton1_Click(object sender, EventArgs e) =>
+            Process.Start("https://drivers.amd.com/drivers/whql-amd-software-adrenalin-edition-22.11.2-win10-win11-dec8.exe");
+        //
+        // End
+        //
+
+        //
+        // MPO Switch
+        //
+        private void materialSwitch1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!Ready) return;
+            if (materialSwitch1.Checked)
+                defaultKey.SetValue("OverlayTestMode", 0x00000005, RegistryValueKind.DWord);
+            else
+                defaultKey.DeleteValue("OverlayTestMode");
+            materialFloatingActionButton2.Visible = true;
+        }
+        //
+        // End
+        //
+
+        //
+        // Reboot PC Button
+        //
+        private void materialFloatingActionButton2_Click(object sender, EventArgs e) =>
+            Process.Start("ShutDown", "/r");
+        //
+        // End
+        //
+    }
+}
